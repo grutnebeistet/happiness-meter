8393ee094044b214fb8ea8e5d3ea9b9f6e80580e
Grutnebeistet <adrianfroberts@gmail.com>
2020-04-01 00:38:32 +0200

    Add Firebase Android/iOS

--- lib/main.dart
+++ lib/main.dart
@@ -150,7 +150,7 @@ class _MyAppState extends State<MyApp> with SingleTickerProviderStateMixin {
                                       Tab(
                                         text: allTranslations
                                             .text('main.tab_meter'),
-                                        icon: Icon(HappMeter.meter_tab_png,
+                                        icon: Icon(HappMeter.meter_tab,
                                             size: 40),
                                       ),
                                       Tab(
